3f17142ddcc7445a25e8b4e4558f1b1d87cd31ca
waffelhaus92 <cory_martinez@rocketmail.com>
2024-02-17 20:35:10 -0500

    first push to github

---
+++ eyes.js
@@ -0,0 +1,13 @@
+//wathes for mouse movement
+const balls = document.getElementsByClassName('ball');
+document.onmousemove = (event) => {
+  const x = (event.clientX * 100) / window.innerWidth + '%';
+  const y = (event.clientY * 100) / window.innerHeight + '%';
+
+//for loop for the axis movement of the 'pupils'
+  for (let i = 0; i < 2; i++) {
+    balls[i].style.left = x;
+    balls[i].style.top = y;
+    balls[i].transform = 'translate(-' + x + ',-' + y + ')';
+  }
+};
